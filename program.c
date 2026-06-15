6d1e758d6f99feb4ad1aca07d3eef392b98bf14a
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2026-06-15 20:38:16 +0530

    Grid generation

--- program.c
+++ program.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+Unopened: -
+Opened: 0-8
+Mark: x
+Mine: +
+*/
+
+void initializeGrid(int x, int y, int grid[][x]) {
+    for (int i = 0; i < y; i++)
+        for (int j = 0; j < x; j++)
+            grid[i][j] = rand() % 5 == 0 ? 1 : 0;
+}
+        
+void displayGrid(int x, int y, int grid[][x]) {
+    printf("\n");
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            printf("%d ", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 void play() {
-    //
+    srand(time(0));
+    int lengthX = 8, lengthY = 6;
+    int grid[lengthY][lengthX];
+
+    initializeGrid(lengthX, lengthY, grid);
+    displayGrid(lengthX, lengthY, grid);
 }
 
 void main() {
