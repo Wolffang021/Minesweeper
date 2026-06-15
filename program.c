4f33b691a18d2fe307a2bac2bd340c4646c982cb
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2026-06-15 19:37:02 +0530

    Base menu loop

--- program.c
+++ program.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+void play() {
+    //
+}
 
 void main() {
-    printf("Hello, World!");
+    bool exit = false;
+    
+    do {
+        char choice;
+        printf("\nMINESWEEPER");
+        printf("\n\tby Shourjo");
+        printf("\nPLAY [Press '1']");
+        printf("\nEXIT [Press '0']");
+        printf("\n\nInput: ");
+        scanf(" %c", &choice);
+
+        switch (choice) {
+            default:
+                printf("\n!!INVALID INPUT!!\n");
+                break;
+            
+            case '0':
+                exit = true;
+                break;
+
+            case '1':
+                play();
+                break;
+        }
+    } while (!exit);
+
+    printf("\nHope you had fun! <3\n");
 }
