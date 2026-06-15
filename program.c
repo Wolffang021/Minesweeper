65730da68d75c25ba99ba00d5222ef4b8370c88f
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2026-06-15 21:06:49 +0530

    Grid display worked

--- program.c
+++ program.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 /*
@@ -10,29 +11,60 @@ Mark: x
 Mine: +
 */
 
-void initializeGrid(int x, int y, int grid[][x]) {
-    for (int i = 0; i < y; i++)
-        for (int j = 0; j < x; j++)
-            grid[i][j] = rand() % 5 == 0 ? 1 : 0;
-}
-        
-void displayGrid(int x, int y, int grid[][x]) {
-    printf("\n");
+void initializeGrid(int x, int y, int grid[][x], char display[][x]) {
     for (int i = 0; i < y; i++) {
         for (int j = 0; j < x; j++) {
-            printf("%d ", grid[i][j]);
+            grid[i][j] = rand() % 5 == 0 ? 1 : 0;
+            display[i][j] = '-';
         }
+    }
+}
+        
+void displayGrid(int x, int y, char display[][x]) {
+    printf("\n");
+    for (int i = y - 1; i >= -1; i--) {
+        if (i == -1) {
+            printf("  ");
+            for (int j = 0; j < x; j++)
+                printf("%d ", j);
+            continue;
+        }
+
+        printf("%d ", i);
+        for (int j = 0; j < x; j++)
+            printf("%c ", display[i][j]);
+        
         printf("\n");
     }
+    printf("\n");
 }
 
 void play() {
     srand(time(0));
     int lengthX = 8, lengthY = 6;
     int grid[lengthY][lengthX];
+    char display[lengthY][lengthX];
 
-    initializeGrid(lengthX, lengthY, grid);
-    displayGrid(lengthX, lengthY, grid);
+    initializeGrid(lengthX, lengthY, grid, display);
+
+    bool exit = false;
+    do {
+        char choice[1024];
+        displayGrid(lengthX, lengthY, display);
+        printf("\nOpen unopened cell: [x-coord]<space>[y-coord]");
+        printf("\nMark cell: [m]");
+        printf("\nExit: [x]");
+        printf("\nInput: ");
+        gets(choice);
+
+        switch (choice) {
+            default:
+                printf("\n!!INVALID INPUT!!\n");
+                break;
+            
+            case 
+        }
+    } while (!exit);
 }
 
 void main() {
